f2026916156c486a517fde4cfd4cf4cedcab9ba7
Muhammad Shoaib <mshoaibkaglur@gmail.com>
2025-04-22 18:54:08 +0500

    asa

--- core/settings.py
+++ core/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = 'django-insecure-9rg@-rs5&=whgp0g(lgujhd!1t++&a(7()7vtc7%qe5gnw^$95
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
